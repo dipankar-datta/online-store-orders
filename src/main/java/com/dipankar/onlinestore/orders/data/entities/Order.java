3bdd78e25fcc13c514820e69c80f07051f72b4ab
Dipankar Datta <dattapro5@gmail.com>
2025-07-08 23:49:40 +0530

    Implemented table mapping

--- src/main/java/com/dipankar/onlinestore/orders/data/entities/Order.java
+++ src/main/java/com/dipankar/onlinestore/orders/data/entities/Order.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -12,11 +13,19 @@ import java.util.List;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "orders")
 public class Order {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private Long customerId;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<OrderItem> orderItems;
+
     private Double totalAmount;
     private LocalDateTime orderDate;
     private String status;
--- src/main/java/com/dipankar/onlinestore/orders/data/entities/OrderItem.java
+++ src/main/java/com/dipankar/onlinestore/orders/data/entities/OrderItem.java
@@ -5,14 +5,25 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import jakarta.persistence.*;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "order_item")
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private Long productId;
     private Double pricePerUnit;
     private Integer quantity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id")
+    private Order order;
 }
